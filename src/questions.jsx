af0733717a1f93871971b448c8070bee03174d1e
juanillo <juanillojohnmichael7@gmail.com>
2024-11-19 08:02:12 +0800

    first commit

---
+++ src/questions.jsx
@@ -0,0 +1,134 @@
+export const questions = [
+  {
+    questions_aid: 1,
+    question_question: "q1 Maganda ba ako?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questions_aid: 2,
+    question_question: "q2 Papasa ba ako kay Ma'am Khate?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Asa ka pa",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    questions_aid: 3,
+    question_question: "q3 Mahal ba ako ni Ma'am Ayaca",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: true,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Asa ka pa",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questions_aid: 4,
+    question_question: "q4 minit ba ang ice cream?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    questions_aid: 5,
+    question_question: "q5 malamig ba ang sopas?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    questions_aid: 6,
+    question_question: "q6 malamig ba ang sopas?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      },
+    ],
+  },
+];
